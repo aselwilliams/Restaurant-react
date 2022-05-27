0fa16f624a6bd51ce87fa4595bc41caf6debc597
aselwilliams <aselwilliams22@gmail.com>
2022-05-27 13:19:58 -0400

    formatted the code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import data from './data'
-import './App.css';
-import {useState,useEffect} from 'react';
-import Menu from './components/Menu';
-import Categories from './components/Categories';
+import data from "./data";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
 
 function App() {
-const [menuList, setMenuList] =useState(data)
-const [filteredMenu, setFilteredMenu] = useState([])
-console.log(data)
+  const [menuList, setMenuList] = useState(data);
+  const [filteredMenu, setFilteredMenu] = useState([]);
+  console.log(data);
 
-useEffect(()=>{
-  setFilteredMenu(menuList)
-},[])
+  useEffect(() => {
+    setFilteredMenu(menuList);
+  }, []);
 
-const handleCategory=(action)=>{
-console.log(action)
-if(action==='all') {
-  setFilteredMenu(menuList)
-} else{
-  const menuCopy=[...menuList]
-  const newList=menuCopy.filter((item)=>item.category===action)
-  setFilteredMenu(newList)
-}
-}
+  const handleCategory = (action) => {
+    console.log(action);
+    if (action === "all") {
+      setFilteredMenu(menuList);
+    } else {
+      const menuCopy = [...menuList];
+      const newList = menuCopy.filter((item) => item.category === action);
+      setFilteredMenu(newList);
+    }
+  };
   return (
     <div className="menu">
-        <header className='title'>
+      <header className="title">
         <h1>Menu</h1>
-        <div className='underline'></div>
-        </header>
-        <Categories handleCategory={handleCategory} />
-        <Menu menuList={menuList} filteredMenu={filteredMenu} />
-    
+        <div className="underline"></div>
+      </header>
+      <Categories handleCategory={handleCategory} />
+      <Menu menuList={menuList} filteredMenu={filteredMenu} />
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function Menu({filteredMenu}) {
+function Menu({ filteredMenu }) {
   return (
-    
-    <div className='item-location'>
-        {filteredMenu.map((item)=>{
-            return (
-                <div key={item.id} className='menu-item'>
-                <img className='photo' src={item.img} alt={item.title} />
-                <div className='item-info'>
-                <header className='item-header'>
-                    <h3 className='title'>{item.title}</h3>
-                    <h3 className='price'>$ {item.price}</h3>
-                </header>
-                <p className='item-text'>{item.desc}</p>
-                </div>
-                </div>
-            )
-        })}
-      
-    </div> 
-  )
+    <div className="item-location">
+      {filteredMenu.map((item) => {
+        return (
+          <div key={item.id} className="menu-item">
+            <img className="photo" src={item.img} alt={item.title} />
+            <div className="item-info">
+              <header className="item-header">
+                <h3 className="title">{item.title}</h3>
+                <h3 className="price">$ {item.price}</h3>
+              </header>
+              <p className="item-text">{item.desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
